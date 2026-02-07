92d7e346b664b0d71b3b27b09a9d3bf9ad93cff1
Mosab_Ali <musabmax779@gmail.com>
2026-02-07 05:40:36 +0300

    added test data and created dimension table for customers, also updated the source file for bronze layer and added new files for silver and gold layers.

--- DBT_SNOWFLAKE/analyses/test_data.sql
+++ DBT_SNOWFLAKE/analyses/test_data.sql
@@ -1,21 +1,15 @@
 
-SELECT
-ROW_NUMBER() OVER (ORDER BY CST_ID) AS customer_key,
-ci.CST_ID AS Customer_id,
-ci.CST_KEY AS Customer_number,
-ci.CST_FIRSTNAME AS First_name,
-ci.CST_LASTNAME AS Last_name,
-la.CNTRY AS Country ,
-ci.CST_MARITAL_STATUS AS Marital_status,
+
+SELECT DISTINCT 
+ci.CST_GNDR,
+ca.GEN,
 CASE 
     WHEN ci.CST_GNDR != 'Unknown' THEN ci.CST_GNDR   --- CRM IS THE MASTER FOR GENDER INFORMATION
         ELSE COALESCE(ca.GEN, 'n/a')
-END AS gender ,
-ca.BDATE AS Birthdate,
-ci.CST_CREATE_DATE AS Create_date
+END AS new_gen                                                   
 FROM {{ ref('silver_crm_cust_info') }} ci 
 LEFT JOIN {{ ref('silver_erp_cust_az12') }} ca 
 ON ci.CST_KEY = ca.CID 
 LEFT JOIN {{ ref('silver_erp_loc_a101') }} la
 ON ci.CST_KEY = la.CID
-
+ORDER BY 1,2
